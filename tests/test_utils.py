13f0dff14cf3775fca711cd544e6f0c807a0848e
Gluyki <betonbar@bk.ru>
2026-03-31 20:35:37 +0300

    update test_utils.py with test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,3 +16,10 @@ def test_read_json_try(utils_json):
 def test_transaction_withdrawal_rub(utils_json):
     assert transaction_withdrawal(441945886) == '31957.58'
     assert transaction_withdrawal(445) == 'id не найден'
+
+
+def test_read_json_except():
+    with patch('json.load', side_effect = json.JSONDecodeError('', '', 0)):
+           assert read_json() == []
+
+
