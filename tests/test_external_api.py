af8eb6c614fdf8c21f0d34b81fbaba67c12e64d7
Gluyki <betonbar@bk.ru>
2026-03-31 20:38:46 +0300

    update test_external_api.py

---
+++ tests/test_external_api.py
@@ -0,0 +1,8 @@
+from src.external_api import conversion
+from unittest.mock import patch
+
+
+@patch('requests.get')
+def test_conversion(mock_test, utils_json):
+    mock_test.return_value.json.return_value = utils_json
+    assert conversion('', '') == utils_json
