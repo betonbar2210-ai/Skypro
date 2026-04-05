39e1a4f00d903c50cb18ad811c8f998fc11da500
Gluyki <betonbar@bk.ru>
2026-04-05 21:44:30 +0300

    add test_reader_csv_xlsl.py

--- src/reader_csv_xlsl.py
+++ src/reader_csv_xlsl.py
@@ -1,21 +1,20 @@
 import csv
-import pandas as pd
 
+
+import pandas as pd
 
 
 def reader_csv(way_csv):
     """Функция чтения cvs файла вернуть список словарей"""
-    with open(way_csv, encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter= ';')
+    with open(way_csv, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
         data_csv = []
         for row in reader:
             data_csv.append(row)
         return data_csv
 
 
-def reader_exel(way_excel):
+def reader_excel(way_excel):
     """Функция чтения EXCEL файла с помощью pandas"""
     data_excel = pd.read_excel(way_excel)
     return data_excel
-
-
---
+++ tests/test_reader_csv_xlsl.py
@@ -0,0 +1,19 @@
+import csv
+import os
+from unittest.mock import Mock, patch
+
+from config import ROOT_DIR
+from src.reader_csv_xlsl import reader_excel, reader_csv
+
+
+def test_reader_csv(test_csv):
+    way_csv = os.path.join(ROOT_DIR, "data", "transactions.csv")
+    mock_csv = Mock(return_value=test_csv)
+    csv.DictReader = mock_csv
+    assert reader_csv(way_csv) == test_csv
+
+
+@patch("pandas.read_excel")
+def test_reader_excel(mock_excel, test_excel):
+    mock_excel.return_value = test_excel
+    assert reader_excel("").to_dict() == test_excel.to_dict()
